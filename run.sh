eac72105dcb027cf5d34a11a877faadf11247133
Jose Narvaez <josenarvaez@utez.edu.mx>
2022-09-03 18:54:46 +0000

    add sleep

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ docker stop api-rest || true && docker rm api-rest || true
 # run docker app
 docker run -itd --name api-rest -p 3000:3000 josenarvaezfigueroa/miappcdk-ago22:1.0
 
+sleep 20
+
 docker logs api-rest
